5d6295b40f091f478c02d7752c1daf8d722ecd28
nuttaponkhamkul123 <aladen0009@gmail.com>
2021-11-06 21:14:28 +0700

    added dynamic title name link with router name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { Router,NavigationEnd,Event } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { Subscription  } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'crud-proj';
+  public routerName! : String
+  constructor(private router : Router,){
+    this.router.events.subscribe((event:Event) => {
+      if(event instanceof NavigationEnd ){
+        console.log(event.url);
+        this.routerName! = event.url
+      }
+    });
+  }
+  ngOnInit() : void{
+      console.log(this.router.url)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,4 +29,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule  {
+
+    
+
+ }
