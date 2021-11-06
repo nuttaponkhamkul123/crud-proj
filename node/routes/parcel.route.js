e8f3bf9cf23000cd5b9545cc16bad0b59fa8dc59
nuttaponkhamkul123 <aladen0009@gmail.com>
2021-11-06 11:52:46 +0700

    added route to node

---
+++ node/routes/parcel.route.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+
+
+const parcelRoute = express.Router();
+let Parcel = require('../model/Parcel');
+
+
+//Add Parcel
+parcelRoute.route('add-parcel').post((req,res,next) =>{
+    Parcel.create(req.body, (err,data) => {
+        if(err) return next(err);
+        else res.json(data)
+    })
+})
+
+
+//fetch all Parcels
+
+parcelRoute.route('/').get((req,res) =>{
+    Parcel.find((err,data) => {
+        if(err) return next(err);
+        else res.json.data;
+    })
+})
+
+//get specific book
+
+parcelRoute.route('/fetch-parcel/:id').get((req,res) =>{
+    Parcel.findById(req.params.id,(err,data) => {
+        if(err) return next(err);
+        else res.json.data;
+    })
+})
+
+//update parcel data
+parcelRoute.route('/update-parcel/:id').put((req,res,next) =>{
+    Parcel.findByIdAndUpdate(req.params.id,{
+        $set : req.body
+    }, (err,data) =>{
+        if(err) return next(err);
+        else{
+            res.json(data);
+            console.log("Parcel Updated");
+        }
+    })
+})
+
+
+//delete parcel
+
+parcelRoute.route('/delete-parcel/:id').delete((req,res,next) =>{
+    Parcel.findByIdAndRemove(req.params.id,(err,data) => {
+        if(err) return next(err);
+        else res.status(200).json({
+            msg : data
+        });
+    })
+})
+
+module.export = parcelRoute;
